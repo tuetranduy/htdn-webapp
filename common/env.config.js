e4d067e89a7798ca2d4236c392fd871433aedf0d
tuetranduy <tue1996@gmail.com>
2018-11-15 22:05:33 +0700

    fix SECRET_KEY for prod

--- common/env.config.js
+++ common/env.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   production: {
     API_URL: "https://htdn-api.herokuapp.com/",
-    SECRET_KEY: "9fc83403-e2cb-4053-8328-28945511fddf"
+    SECRET_KEY: "7d0d383d-cb35-41b6-aa1b-c282054668c5"
   },
   development: {
     API_URL: "http://localhost:3001/",
